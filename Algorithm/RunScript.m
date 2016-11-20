24af7acb017691cb1a980d5bb5d5d83c17269b94
Reid Anderson <and02911@umn.edu>
2016-11-19 23:42:45 -0600

    Fix a bug with the vibrational intensity, and bring latest local changes up

--- Algorithm/RunScript.m
+++ Algorithm/RunScript.m
@@ -2,13 +2,18 @@ T = [250:350];
 % T = [250];
 T = [T 500 625 750 875 1000 1125 1250 1375 1500];
 %T = [3300];
-molAbbrev = 'Naphthalene';
+molAbbrev = 'ACN';
+% Have we already computed the DOS for this run?
 DOSExists = 0;
-runName = '9-6-2016-IRInt1mil';
+runName = '9-5-2016-IRInt1mil';
+% How many steps do we want to take in the Monte Carlo that computes the spectra?
 IRSteps = 1000000;
+% How many iterations of the WL do we want to perform, and what's the maximum number of steps in each?
 DOSIters = 20;
 DOSSteps = 2000000;
-raman = 0;
+% Should we compute the Raman spectra instead of the IR spectra?
+raman = 1;
+% How many independent runs do we want to average?
 numCores = 1;
 WLRun(molAbbrev,T,DOSExists,runName,IRSteps,DOSIters,DOSSteps,raman,numCores);
 molAbbrev = '';
