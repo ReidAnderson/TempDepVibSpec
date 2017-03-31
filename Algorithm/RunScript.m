8472d218d81997c7079471f0043f24c3c904146e
Reid Anderson <and02911@umn.edu>
2017-03-30 23:25:19 -0500

    Update script to run for CHCl3

--- Algorithm/RunScript.m
+++ Algorithm/RunScript.m
@@ -2,12 +2,12 @@ T = [250:350];
 % T = [250];
 T = [T 500 625 750 875 1000 1125 1250 1375 1500];
 %T = [3300];
-molAbbrev = 'CO2';
+molAbbrev = 'CHCl3';
 % Have we already computed the DOS for this run?
-DOSExists = 1;
-runName = '3-27-2017-Raman';
+DOSExists = 0;
+runName = '3-30-2017-Raman';
 % How many steps do we want to take in the Monte Carlo that computes the spectra?
-IRSteps = 100000;
+IRSteps = 1000000;
 % How many iterations of the WL do we want to perform, and what's the maximum number of steps in each?
 DOSIters = 20;
 DOSSteps = 2000000;
